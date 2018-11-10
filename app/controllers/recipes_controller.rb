7064061119262bb3716c797d7d223c90e55d0257
John Rockhead <jrockhea@uncc.edu>
2018-11-10 00:07:04 +0000

    Complete Create and Show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,25 @@
 class RecipesController < ApplicationController
+    def index
+        @recipes = Recipebook.all
+    end
+    
+    def show
+        @recipe = Recipebook.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:recipe].inspect
+        @recipe = Recipebook.new(recipe_params)
+        
+        @recipe.save
+        redirect_to action: "index" 
     end
 end
+
+private 
+    def recipe_params
+        params.require(:recipe).permit(:meal, :ingredients)
+    end
+
