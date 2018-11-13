d7f4db9db2179715b7ff6c8eb70b7202050fbe70
Cameron King <cking107@uncc.edu>
2018-11-13 22:18:40 +0000

    Added edit and delete actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,27 @@ class RecipesController < ApplicationController
         @recipe = Recipebook.new(recipe_params)
         
         @recipe.save
-        redirect_to action: "index" 
+        redirect_to action: "index"
+    end
+    
+    def edit
+        @recipe = Recipebook.find(params[:id])
+    end
+    
+    def update
+        @recipe = Recipebook.find(params[:id])
+        if @recipe.update(recipe_params)
+            redirect_to action: "show"
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @recipe = Recipebook.find(params[:id])
+        @recipe.destroy
+        
+        redirect_to recipes_path
     end
 end
 
